e75354be9a3c5a87e4803d95b024a1e4e08f9a9f
CESAR NAVARRO <34008442+seonatic@users.noreply.github.com>
2018-06-17 13:20:07 +0200

    v1.1

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,4 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./keys_prod');
-} else {
-  module.exports = require('./keys_dev');
-}
+module.exports = {
+    mongoURI:'mongodb://test:test123@ds155730.mlab.com:55730/youtubelist',
+    secretOrKey: 'secret'
+};
